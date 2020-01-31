591584ce84483b9efb9f386aa2efcb5edf59eccf
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-01-31 07:51:50 +0000

    Added Random Movie Quotes

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ const lib = require('thoughts');
 var myThoughts = lib.random();
 console.log(myThoughts);
 
+const Movie = require('popular-movie-quotes');
+var myMovie = Movie.getRandomQuote();
+console.log(myMovie); 
+
 //copy information to a file
 const fs = require("fs");
 fs.writeFileSync("file1.txt", mySuperHeroName);
 fs.writeFileSync("file2.txt", mySuperVillainName);
 fs.writeFileSync("file3.txt", myQuote);
+fs.writeFileSync("file4.txt", myMovie);
