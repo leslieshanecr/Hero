b76456872e4014ff586f9a0b641eceda21cee05a
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-01-31 08:51:31 +0000

    Added comments to code

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
+// adds packages for superheroes and supervillains
 const superheroes = require('superheroes');
 const supervillains = require('supervillains');
 
+//saves two variables of a random hero and villian
 var mySuperHeroName = superheroes.random();
 var mySuperVillainName = supervillains.random();
 
+//displays the hero and villain to the console
 console.log(mySuperHeroName);
 console.log(mySuperVillainName);
 
+// adds package for inspiration quote and displays it
 const Quote = require('inspirational-quotes');
 var myQuote = Quote.getRandomQuote();
 console.log(myQuote);
 
+//adds package for thoughts and displays it
 const lib = require('thoughts');
 var myThoughts = lib.random();
 console.log(myThoughts);
 
+//add package for popular movies and displays it
 const Movie = require('popular-movie-quotes');
 var myMovie = Movie.getRandomQuote();
 console.log(myMovie); 
 
-//copy information to a file
+//copy above information to a file
 const fs = require("fs");
 fs.writeFileSync("file1.txt", mySuperHeroName);
 fs.writeFileSync("file2.txt", mySuperVillainName);
